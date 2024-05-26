b5d3d85b726ec6c5a3806c9b0fee9ffb0b6ca7ec
ZakThink <zak@thinkable.co>
2024-05-26 19:08:04 +0100

    initial commit

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,29 @@
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const messageTypeEnum = pgEnum("message_type_enum", ["ai", "user"]);
+
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  pdfName: text("pdf_name").notNull(),
+  pdfUrl: text("pdf_url").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  fileKey: varchar("file_key", { length: 256 }).notNull(),
+});
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatId: integer("chat_id")
+    .references(() => chats.id)
+    .notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  role: messageTypeEnum("role").notNull(),
+});
